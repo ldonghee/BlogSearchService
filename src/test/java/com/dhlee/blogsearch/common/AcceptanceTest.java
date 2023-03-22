af29a90122bbeddda484769f37efc7961f71617c
donghui.lee <donghui.lee@bugs.co.kr>
2023-03-22 15:15:48 +0900

    test: 블로그 검색 인수테스트 추가

---
+++ src/test/java/com/dhlee/blogsearch/common/AcceptanceTest.java
@@ -0,0 +1,20 @@
+package com.dhlee.blogsearch.common;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class AcceptanceTest {
+	@LocalServerPort
+	int port;
+
+	@BeforeEach
+	public void setUp() {
+		if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+			RestAssured.port = port;
+		}
+	}
+}
